d76782f50196dc0322a31440c093dc2d5524bacf
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2023-07-31 21:12:38 +0530

    admin api route done

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,9 +1,8 @@
 const Product = require("../models/productModels");
 
-//create a new product
-exports.createProduct = async (req, res) => {
+//CREATE A NEW PRODUCTS
+exports.createProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
-  
 
   res.status(201).json({
     success: true,
@@ -12,6 +11,78 @@ exports.createProduct = async (req, res) => {
   });
 };
 
-exports.getAllProduct = (req, res) => {
-  res.status(200).json({ message: "Your route is running okay" });
+// UPDATE PRODUCTS
+
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "product id not found ! pls enter a valid product id ",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true, // to get the updated data in response
+    runValidator: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+    message: "product uppdated succesfully",
+  });
+};
+
+// GET ALL PRODUCTS
+exports.getAllProduct = async (req, res) => {
+  const allProducts = await Product.find({});
+  res.status(200).json({
+    success: "true",
+    allProducts,
+    message: "all products fetch succesfully",
+  });
+};
+
+//GET PRODUCT DETAILS- BY ID
+
+exports.productDetails = async (req, res) => {
+    let product = await Product.findById(req.params.id);
+  
+    if (!product) {
+      return res.status(500).json({
+        success: false,
+        message: "product id not found ! pls enter a valid product id ",
+      });
+    }
+  
+    product= await Product.findById(req.params.id);
+  
+    res.status(200).json({
+      success: true,
+      product,
+      message: "products details fetch succesfully od particular id",
+    });
+  };
+
+// DELET ALL PRODUCTS
+
+exports.deleteProducts = async (req, res) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "product id not found ! pls enter a valid product id ",
+    });
+  }
+
+  await Product.findOneAndDelete(req.params.id);
+
+  res.status(200).json({
+    success: true,
+    message: "product delete succesfully",
+  });
 };
--- Backend/routes/getProductsRoutes.js
+++ Backend/routes/getProductsRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { getAllProduct, createProduct } = require("../controllers/productController");
+const { getAllProduct, createProduct, updateProduct, deleteProducts } = require("../controllers/productController");
 const router = express.Router();
 router.route("/products").get(getAllProduct);
 router.route("/products/new").post(createProduct);
+router.route("/products/:id").put(updateProduct);
+router.route("/products/:id").delete(deleteProducts);
+router.route("/products/:id").get(updateProduct); // GET PRODUCTS BY ID
 
 
 module.exports = router;
